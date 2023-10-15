e604f61a91a726f8bb1c0849d56bc7dad6e87850
Deepthi106 <147964631+Deepthi106@users.noreply.github.com>
2023-10-15 10:37:28 +0530

    Add files via upload

---
+++ day1/FindNumbers.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class FindNumbers {
+
+	public static void main(String[] args) {
+		
+		int givenNumber = -45;
+		
+		if(givenNumber>0)
+		{
+			System.out.println("The given number is positive");
+		}
+		else {
+			System.out.println("The given number is negative");	
+		}
+	}
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+     float browserVersion = 100.2f;
+     String browserName = "firefox";
+     boolean pageLoaded = true;
+     int onOfBrowser = 1998;
+     char browserLogo='f';
+     
+     System.out.println("Display values:  "+browserVersion);
+     System.out.println("Browser Name: "+browserName);
+     System.out.println("Page Loaded: " + pageLoaded);
+     System.out.println("onOf Browser: "+onOfBrowser);
+     System.out.println("Browser Logo: "+browserLogo); 
+    
+	}
+
+}
+
